c3e850debfe88b1797f8cd98bb66f9f0b5c6f640
Paula Pizarro <paulapizaru@gmail.com>
2025-04-15 16:30:42 -0400

    Restructuring exercise names
    To improve clarity.

---
+++ semana_1/tests/01-Title-Exercise.py
@@ -0,0 +1,15 @@
+# Import re module for regular expressions 
+import re
+from playwright.sync_api import Page, expect
+
+# Define the url outside
+url = "https://www.google.cl"
+
+
+# Define the test
+def test_has_title(page: Page):
+    # We tell the page to load the specified url.
+    page.goto(url)
+
+    # We expect to find the word "Google" in the title of the loaded page.
+    expect(page).to_have_title(re.compile("Google"))
---
+++ semana_1/tests/02-GoogleSearch-Exercise.py
@@ -0,0 +1,26 @@
+# Testing google #
+
+# Import re module for regular expressions 
+import re
+from playwright.sync_api import Page, expect
+
+# Define the url outside
+url = "https://www.google.cl"
+
+def test_search(page: Page):
+    # This is to start tracing the steps of the test, taking snapshots
+    page.context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    page.goto(url)
+
+    expect(page).to_have_title(re.compile("Google"))
+
+    # We tell the page to fill the input with the specified selector with the word "Playwright"
+    page.fill('#APjFqb',"Playwright")
+
+    # We press "Enter" in our keyboard to recreate the search.
+    page.keyboard.press("Enter")
+
+    # This is to stop tracing and also where the snapshots are going to be stored.
+    page.context.tracing.stop(path="trace.zip")
+
