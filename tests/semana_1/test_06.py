d14ebfdbb7186b7cf3305caf72ca6f878eac5dc5
Paula Pizarro <paulapizaru@gmail.com>
2025-04-19 11:43:10 -0400

    Exercise day 6

---
+++ tests/semana_1/test_06.py
@@ -0,0 +1,16 @@
+import re
+from playwright.sync_api import Page, expect
+
+url = "https://the-internet.herokuapp.com/dynamic_loading/1"
+
+def test_wait(page:Page):
+    page.goto(url)
+    expect(page).to_have_title("The Internet")
+
+    page.get_by_role("button", name="Start").click()
+    
+    # Wait for the text to be visible
+    page.get_by_text("Hello World!").wait_for(state="visible")
+
+    # Assertion
+    expect(page.get_by_text("Hello World!")).to_be_visible()
