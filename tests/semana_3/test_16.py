1ee684cd838330b931ecd8faa32333482c267b69
Paula Pizarro <paulapizaru@gmail.com>
2025-04-29 22:05:42 -0400

    Exercise day 16
    I need to fix it

---
+++ tests/semana_3/test_16.py
@@ -0,0 +1,27 @@
+# Test Exercise N°15
+# 
+import os
+from playwright.sync_api import Page, expect
+
+url = "https://the-internet.herokuapp.com/upload"
+
+def test_file_upload(page:Page):
+    page.goto(url, wait_until="domcontentloaded")
+
+    expect(page.get_by_role("heading", name="File Uploader")).to_be_visible()
+
+    # Get the file path
+    current_dir = os.path.dirname(__file__)
+    file_path =  os.path.abspath(os.path.join(current_dir, "..", "..", "..", "files", "test_file.txt"))
+
+    # Set file to input
+    file_input = page.locator("input#file-upload")
+    file_input.wait_for(state="visible")
+    file_input.set_input_files(file_path)
+
+    # Click Upload
+    page.click("input#file-submit")
+
+    # Assertions
+    expect(page.locator("h3").to_have_text("File Uploaded!"))
+    expect(page.locator("#uploaded-files")).to_have_text("test_file.txt")
